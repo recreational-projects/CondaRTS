ae680a0ec2f724323c7a93ae756475f1bad65662
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-09-09 10:34:32 +0100

    refactor: FogOfWar class to module

--- CondaRTS.py
+++ CondaRTS.py
@@ -4,11 +4,11 @@ import random
 import pygame
 
 from src.camera import Camera
-from src.constants import CONSOLE_HEIGHT, MAP_HEIGHT, MAP_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH
+from src.constants import CONSOLE_HEIGHT, MAP_HEIGHT, MAP_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH, TILE_SIZE
+from src.fog_of_war import FogOfWar
 from src.game_object import GameObject
 from src.particle import Particle
 
-TILE_SIZE = 32
 BUILDING_RANGE = 160
 BASE_PRODUCTION_TIME = 180
 POWER_PER_PLANT = 100
@@ -165,76 +165,6 @@ def handle_projectiles(projectiles, all_units, buildings):
         # Only kill projectile if it has no valid target or moves too far
         elif not (projectile.target and hasattr(projectile.target, "health") and projectile.target.health > 0):
             projectile.kill()
-
-
-class FogOfWar:
-    def __init__(self, map_width, map_height, tile_size=TILE_SIZE):
-        self.tile_size = tile_size
-        self.explored = [[False] * (map_height // tile_size) for _ in range(map_width // tile_size)]
-        self.visible = [[False] * (map_height // tile_size) for _ in range(map_width // tile_size)]
-        self.surface = pygame.Surface((map_width, map_height), pygame.SRCALPHA)
-        self.surface.fill((0, 0, 0, 255))
-
-    def reveal(self, center, radius):
-        cx, cy = center
-        tile_x, tile_y = cx // self.tile_size, cy // self.tile_size
-        radius_tiles = radius // self.tile_size
-        for y in range(
-            max(0, tile_y - radius_tiles),
-            min(len(self.explored[0]), tile_y + radius_tiles + 1),
-        ):
-            for x in range(max(0, tile_x - radius_tiles), min(len(self.explored), tile_x + radius_tiles + 1)):
-                if (
-                    math.sqrt(
-                        (cx - (x * self.tile_size + self.tile_size // 2)) ** 2
-                        + (cy - (y * self.tile_size + self.tile_size // 2)) ** 2
-                    )
-                    <= radius
-                ):
-                    self.explored[x][y] = True
-                    self.visible[x][y] = True
-
-    def update_visibility(self, units, buildings, team):
-        self.visible = [[False] * len(self.explored[0]) for _ in range(len(self.explored))]
-        for unit in units:
-            if unit.team == team and hasattr(unit, "rect"):
-                self.reveal(unit.rect.center, 150)
-        for building in buildings:
-            if building.team == team and hasattr(building, "rect"):
-                self.reveal(building.rect.center, 200)
-            if hasattr(building, "rect") and building.health > 0:
-                tile_x, tile_y = (
-                    int(building.rect.centerx // self.tile_size),
-                    int(building.rect.centery // self.tile_size),
-                )
-                if 0 <= tile_x < len(self.visible) and 0 <= tile_y < len(self.visible[0]):
-                    self.visible[tile_x][tile_y] = True
-                    building.is_seen = True
-
-    def is_tile_visible(self, x, y):
-        tile_x, tile_y = int(x // self.tile_size), int(y // self.tile_size)
-        if 0 <= tile_x < len(self.visible) and 0 <= tile_y < len(self.visible[0]):
-            return self.visible[tile_x][tile_y]
-        return False
-
-    def is_tile_explored(self, x, y):
-        tile_x, tile_y = int(x // self.tile_size), int(y // self.tile_size)
-        if 0 <= tile_x < len(self.explored) and 0 <= tile_y < len(self.explored[0]):
-            return self.explored[tile_x][tile_y]
-        return False
-
-    def draw(self, screen, camera):
-        self.surface.fill((0, 0, 0, 255))
-        for y in range(len(self.explored[0])):
-            for x in range(len(self.explored)):
-                if self.explored[x][y]:
-                    alpha = 0 if self.visible[x][y] else 100
-                    pygame.draw.rect(
-                        self.surface,
-                        (0, 0, 0, alpha),
-                        (x * self.tile_size, y * self.tile_size, self.tile_size, self.tile_size),
-                    )
-        screen.blit(self.surface, (-camera.rect.x, -camera.rect.y))
 
 
 class Tank(GameObject):
